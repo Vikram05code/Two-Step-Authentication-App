e5363bb90f9189c9f1de28c111ec4cfd0943c867
Vikram <vikky5vikram@gmail.com>
2024-03-07 15:03:24 +0530

    initial commit

---
+++ src/main/java/com/vikram/TwoFactorAuthenticationAppApplication.java
@@ -0,0 +1,13 @@
+package com.vikram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoFactorAuthenticationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwoFactorAuthenticationAppApplication.class, args);
+	}
+
+}
