1e8f57cffbe664e6b5c69f316b2e4e34544b0fab
Vikram <vikky5vikram@gmail.com>
2024-03-07 16:18:18 +0530

    Added VerificationRequest and AuthenticationResponse fields

---
+++ src/main/java/com/vikram/Request/VerificationRequest.java
@@ -0,0 +1,13 @@
+package com.vikram.Request;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class VerificationRequest {
+
+	private String email;
+    private String code;
+	
+}
