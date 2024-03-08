8e4c683b43a2178b89bb4a42972c9cd9310aaed4
Vikram <vikky5vikram@gmail.com>
2024-03-09 04:07:48 +0530

    Added EndPoints for Swagger in Security Configuration class

--- src/main/java/com/vikram/config/SecurityConfiguration.java
+++ src/main/java/com/vikram/config/SecurityConfiguration.java
@@ -31,8 +31,16 @@ public class SecurityConfiguration {
                 .disable()
                 .authorizeHttpRequests()
                 .requestMatchers(
-                        "/api/v1/auth/**",
+                		"/api/v1/auth/**",
+                        "/v2/api-docs",
+                        "/v3/api-docs",
+                        "/v3/api-docs/**",
+                        "/swagger-resources",
+                        "/swagger-resources/**",
+                        "/configuration/ui",
+                        "/configuration/security",
                         "/swagger-ui/**",
+                        "/webjars/**",
                         "/swagger-ui.html"
                 )
                 .permitAll()
--- src/main/java/com/vikram/service/AuthenticationService.java
+++ src/main/java/com/vikram/service/AuthenticationService.java
@@ -1,31 +1,12 @@
 package com.vikram.service;
 
 import java.io.IOException;
-
-
-import org.springframework.http.HttpHeaders;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vikram.Request.AuthenticationRequest;
 import com.vikram.Request.RegisterRequest;
 import com.vikram.Request.VerificationRequest;
-import com.vikram.config.JwtService;
-
-import com.vikram.model.User;
 import com.vikram.model.response.AuthenticationResponse;
-import com.vikram.repository.UserRepository;
-import com.vikram.tfa.TwoFactorAuthenticationService;
-
-import jakarta.persistence.EntityNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
-
 
 public interface AuthenticationService {
 
